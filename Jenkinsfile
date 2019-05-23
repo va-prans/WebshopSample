236941e48bf9c299ad99f7ef66c602646b4f277a
master <v@v.v>
2019-05-24 00:56:01 +0200

    j

---
+++ Jenkinsfile
@@ -0,0 +1,9 @@
+node('docker') {
+    stage 'Checkout'
+        checkout scm  
+	stage 'Build'
+	stage 'Unit Test'
+	stage 'Integration Test'
+	stage 'Run'
+        sh "docker-compose -f docker-compose.yml up --build --force-recreate -d"
+}
--- jenkins-docker/master/job.groovy
+++ jenkins-docker/master/job.groovy
@@ -1,14 +1,27 @@
-pipelineJob('webshop') {   
-	def repo = 'https://github.com/va-prans/WebshopSample'
-	properties{ 
-		githubProjectUrl (repo) 
-		rebuild { 
-			autoRebuild(false) 
-		} 
-	}
-	definition {
-		cps { 			 
-			scriptPath('Jenkinsfile')
-		} 
-	} 
+pipelineJob('Your App Pipeline') { 
+
+  def repo = 'https://github.com/va-prans/WebshopSample.git' 
+
+  description("webshop") 
+  keepDependencies(false) 
+
+  properties{ 
+	githubProjectUrl (repo) 
+    rebuild { 
+      autoRebuild(false) 
+    } 
+  } 
+
+  definition { 
+    cpsScm { 
+      scm { 
+        git { 
+          remote { url(repo) } 
+          branches('master') 
+          scriptPath('Jenkinsfile') 
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
+        } 
+      } 
+    } 
+  }
 }

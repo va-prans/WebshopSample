a30231517e39522e6809f0406deb7da93552ccde
master <v@v.v>
2019-05-25 18:45:08 +0200

    nother test

--- WebshopAPI/Controllers/AccountController.cs
+++ WebshopAPI/Controllers/AccountController.cs
@@ -11,7 +11,7 @@ using Webshop.Application.Account.Commands.Update;
 using Webshop.Application.Account.Queries.GetAccountById;
 using Webshop.Application.Address.Commands.Create;
 using Webshop.Application.Address.Commands.Update;
-//just a comment 2
+//just a comment 3
 namespace WebshopAPI.Controllers
 {
     [Produces("application/json")]
